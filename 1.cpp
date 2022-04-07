5777a98bc2a3d1c0905ed1a9f0fa24cc396c5e55
Daria Dehnera <dehnera2020kb12@student.karazin.ua>
2022-04-07 20:29:18 +0300

    Commit after creating Dehnera

--- 1.cpp
+++ 1.cpp
@@ -9,7 +9,7 @@ struct person
  
 int main(void)
 {
-    struct person tom = {24, "Tom"};
+    struct person tom = {25, "Tom"};
     printf("Age: %d \t Name: %s", tom.age, tom.name);
     return 0;
 }
