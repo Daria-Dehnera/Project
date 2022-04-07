e38fb18f5e70b3f6e3d91ea1f76efdfca18d2372
Daria Dehnera <dehnera2020kb12@student.karazin.ua>
2022-04-07 19:22:51 +0300

    First commit

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+struct person
+{
+    int age;
+    char * name;
+};
+ 
+int main(void)
+{
+    struct person tom = {23, "Tom"};
+    printf("Age: %d \t Name: %s", tom.age, tom.name);
+    return 0;
+}
+
